98188691559a3f50ff4a178b16821eacb8af3125
etienne cella <etienne.p.cella@gmail.com>
2020-01-20 17:44:46 -0500

    plug cluster display, fixed exposure

---
+++ Assets/ClusterDisplay/Scripts/KeystrokeActivationToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeystrokeActivationToggle : MonoBehaviour
+{
+    public KeyCode code;
+    public MonoBehaviour behaviour;
+
+    void Update()
+    {
+        if (behaviour == null)
+            return;
+        if (Input.GetKeyDown(code))
+            behaviour.enabled = !behaviour.enabled;
+    }
+}
