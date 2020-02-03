a5d783b4806d54dd80489e694770f57c041a6e3a
etienne cella <etienne.p.cella@gmail.com>
2020-02-03 14:11:18 -0500

    go directly to cinematic mode and stay there, leftCtrl+Q to quit app

---
+++ Assets/ClusterDisplay/Scripts/ExitApp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Q))
+        {
+#if UNITY_EDITOR    
+            UnityEditor.EditorApplication.ExitPlaymode();
+#else    
+            Application.Quit();
+#endif
+        }
+    }
+}
