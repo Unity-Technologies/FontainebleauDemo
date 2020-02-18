c33cf3975d42fb512d2ad8aebf4d2a98ed6c9b26
etienne cella <etienne.p.cella@gmail.com>
2020-02-18 11:13:09 -0500

    cinematic artefact wip

---
+++ Assets/ClusterDisplay/Scripts/ResetVolumetricsOnCut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+[RequireComponent(typeof(CinemachineBrain))]
+public class ResetVolumetricsOnCut : MonoBehaviour
+{
+    CinemachineBrain m_Brain;
+
+    void OnEnable()
+    {
+        m_Brain = GetComponent<CinemachineBrain>();
+        m_Brain.m_CameraCutEvent.AddListener(CutHandler);
+    }
+
+    void OnDisable()
+    {
+        m_Brain.m_CameraCutEvent.RemoveListener(CutHandler);
+    }
+
+    void CutHandler(CinemachineBrain brain)
+    {
+        var cam = brain.OutputCamera;
+        if (cam != null)
+        {
+            HDCamera hdCam = HDCamera.GetOrCreate(cam);
+            hdCam.Reset();
+            hdCam.volumetricHistoryIsValid = false;
+            hdCam.colorPyramidHistoryIsValid = false;
+        }
+    }
+}
---
+++ Assets/ClusterDisplay/Scripts/TimelineLoop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class TimelineLoop : MonoBehaviour
+{
+    PlayableDirector m_Director;
+
+    [SerializeField]
+    float m_Start;
+    [SerializeField]
+    float m_Duration;
+    
+    void OnEnable()
+    {
+        m_Director = GetComponent<PlayableDirector>();
+        StartCoroutine(Loop());
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator Loop()
+    {
+        for (;;)
+        {
+            m_Director.time = m_Start;
+            yield return  new WaitForSeconds(m_Duration);
+        }
+    }
+}
