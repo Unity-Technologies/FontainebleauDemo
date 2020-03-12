48523cb6b3544ae0348fd090ceac144a6f2a2e08
etienne cella <etienne.p.cella@gmail.com>
2020-03-12 17:25:31 -0400

    control timeline speed instead of path speed

---
+++ Assets/ClusterDisplay/Scripts/DirectorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class DirectorController : MonoBehaviour
+{
+    [SerializeField]
+    float m_MaxSpeed;
+    
+    [SerializeField]
+    float m_MinSpeed;
+
+    [SerializeField]
+    float m_SpeedDelta;
+    
+    PlayableDirector m_Director;
+    float m_Speed;
+    
+    void OnEnable()
+    {
+        m_Director = GetComponent<PlayableDirector>();
+        m_Director.RebuildGraph();
+        m_Speed = Mathf.Clamp(1, m_MinSpeed, m_MaxSpeed);
+        m_Director.playableGraph.GetRootPlayable(0).SetSpeed(m_Speed);
+    }
+    
+    void Update()
+    {
+        var delta = 0;
+        if (Input.GetKey(KeyCode.DownArrow))
+            delta = -1;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            delta = 1;
+
+        m_Speed = Mathf.Clamp(m_Speed + delta * m_SpeedDelta, m_MinSpeed, m_MaxSpeed);
+        m_Director.playableGraph.GetRootPlayable(0).SetSpeed(m_Speed);
+    }
+}
