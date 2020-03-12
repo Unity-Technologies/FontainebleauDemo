3c781a879841320a0bd49ec4d786d565e635054d
etienne cella <etienne.p.cella@gmail.com>
2020-03-12 17:42:16 -0400

    implement dolly reset

--- Assets/ClusterDisplay/Scripts/DirectorController.cs
+++ Assets/ClusterDisplay/Scripts/DirectorController.cs
@@ -20,20 +20,30 @@ public class DirectorController : MonoBehaviour
     void OnEnable()
     {
         m_Director = GetComponent<PlayableDirector>();
-        m_Director.RebuildGraph();
         m_Speed = Mathf.Clamp(1, m_MinSpeed, m_MaxSpeed);
+        m_Director.RebuildGraph();
         m_Director.playableGraph.GetRootPlayable(0).SetSpeed(m_Speed);
     }
     
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_Director.time = 0;
+            m_Director.Play();
+            return;
+        }
+
         var delta = 0;
         if (Input.GetKey(KeyCode.DownArrow))
             delta = -1;
         else if (Input.GetKey(KeyCode.UpArrow))
             delta = 1;
-
+        else return;
+        
         m_Speed = Mathf.Clamp(m_Speed + delta * m_SpeedDelta, m_MinSpeed, m_MaxSpeed);
+        if(!m_Director.playableGraph.IsValid())
+            m_Director.RebuildGraph();
         m_Director.playableGraph.GetRootPlayable(0).SetSpeed(m_Speed);
     }
 }
