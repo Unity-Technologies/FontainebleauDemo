f8e0e7d6f8aa8fa253148a24e3926dccebb70d25
TomasKiniulis <tomas.kiniulis@unity3d.com>
2022-09-19 09:21:35 +0300

    update lightmap switch action to apv switch

--- Assets/Scripts/Actions/SwitchLightmapAction.cs
+++ Assets/Scripts/Actions/SwitchLightmapAction.cs
@@ -4,21 +4,25 @@ using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SwitchLightmapAction : ActionBase
 {
     public int LightmapScenarioIndex = 0;
+    public string[] lightingScenarios = { "Day", "Sunset", "Night", "Empty" };
 
     [ReorderableList]
     public Callable[] AfterLoading;
 
     public override void Execute(GameObject instigator = null)
     {
-        var levelLightmapData = FindObjectOfType<LevelLightmapData>();
-        if(levelLightmapData != null)
+        if (ProbeReferenceVolume.instance.isInitialized)
         {
-            levelLightmapData.LoadLightingScenario(LightmapScenarioIndex);
+            ProbeReferenceVolume.instance.SetNumberOfCellsLoadedPerFrame(100);
+            ProbeReferenceVolume.instance.lightingScenario = lightingScenarios[LightmapScenarioIndex];
             Callable.Call(AfterLoading, instigator);
         }
+
     }
 }
+
